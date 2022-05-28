1cc552f03b1f5e0185101c240b168e25009d7b8c
jerbear2008 <jeremyrwoolley@gmail.com>
2022-05-27 21:08:17 -0700

    escape quotes
    agh

--- pages/characters.tsx
+++ pages/characters.tsx
@@ -10,26 +10,26 @@ const Plot: NextPage = () => {
       <p>Ponyboy Michael Curtis is the main character of The Outsiders. He has long light brown hair and greenish-gray eyes. He is smart, but does not use common sense.</p>
       
       <h3>Darrel Curtis</h3>
-      <p>Darrel Shaynne Curtis Jr. is the oldest Curtis boy. He is 6'2" and strong, with dark brown hair and blue-green eyes. He is 20 years old and acts as Ponyboy and Sodapop's guardian. Darrel has a job roofing houses which he does to keep the Curtis family together. He is very street smart, but he appears tough and cold, though he deeply cares about his brothers.</p>
+      <p>Darrel Shaynne Curtis Jr. is the oldest Curtis boy. He is 6&apos;2&quot; and strong, with dark brown hair and blue-green eyes. He is 20 years old and acts as Ponyboy and Sodapop&apos;s guardian. Darrel has a job roofing houses which he does to keep the Curtis family together. He is very street smart, but he appears tough and cold, though he deeply cares about his brothers.</p>
       
       <h3>Sodapop Curtis</h3>
-      <p>Ponyboy Patrick Curtis is the middle child of the Curtis family. He has long dark gold hair and dark brown eyes. He is always happy; it always seems like he is drunk, but he never drinks. He is described as "reckless and thoughtful at the same time". (7) He does not read, and dropped out of high school.</p>
+      <p>Ponyboy Patrick Curtis is the middle child of the Curtis family. He has long dark gold hair and dark brown eyes. He is always happy; it always seems like he is drunk, but he never drinks. He is described as &quot;reckless and thoughtful at the same time&quot;. (7) He does not read, and dropped out of high school.</p>
       
       <h3>Two-Bit Mathews</h3>
-      <p>Keith Mathews, who is always referred to as Two-Bit, is the oldest of the gang, with dark brown sideburns and gray eyes. He is the wisecracker of the group, which is how he got his name, as he "always had to get his two-bits worth in". (9) He treats everything as a joke, and likes to shoplift. His prized possession is his black-handled switchblade.</p>
+      <p>Keith Mathews, who is always referred to as Two-Bit, is the oldest of the gang, with dark brown sideburns and gray eyes. He is the wisecracker of the group, which is how he got his name, as he &quot;always had to get his two-bits worth in&quot;. (9) He treats everything as a joke, and likes to shoplift. His prized possession is his black-handled switchblade.</p>
       
       <h3>Steve Randle</h3>
-      <p>Steve is Sodapop's tough friend. He likes cars and stealing hubcaps. He is smart, tall, and lean.</p>
+      <p>Steve is Sodapop&apos;s tough friend. He likes cars and stealing hubcaps. He is smart, tall, and lean.</p>
       
       <h3>Dallas Winston</h3>
       <p>Dallas Winston, who is called Dally, is the toughest greaser in the gang. He has light blond hair and icy blue eyes. He had been arrested at 10 years old and has a long police record he is proud of. He is protective of Johnny Cade.</p>
       
       <h3>Johnny Cade</h3>
-      <p>Johnny Cade is "the gang's pet, everyone's kid brother". (12) He has black hair and large black eyes. His parents are abusive and alcoholic and do not care about him, so he sees the gang as his real family. He is always nervous and jumpy, and very sensitive.</p>
+      <p>Johnny Cade is &quot;the gang&apos;s pet, everyone&apos;s kid brother&quot;. (12) He has black hair and large black eyes. His parents are abusive and alcoholic and do not care about him, so he sees the gang as his real family. He is always nervous and jumpy, and very sensitive.</p>
       
       <h2>Socs</h2>
       <h3>Cherry Valance</h3>
-      <p>Sherri Valance, called Cherry for her red hair, is a Soc cheerleader. She was Bob's girlfriend. When meeting Dally, she admires him and is afraid to fall in love with him. Cherry feels guilty after Bob attacks Johnny, and acts as a spy for the greasers.</p>
+      <p>Sherri Valance, called Cherry for her red hair, is a Soc cheerleader. She was Bob&apos;s girlfriend. When meeting Dally, she admires him and is afraid to fall in love with him. Cherry feels guilty after Bob attacks Johnny, and acts as a spy for the greasers.</p>
     </Page>
   )
 }
