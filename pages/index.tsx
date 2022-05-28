4840df53a8f6c478218dd2419cce3d4855d13a4e
jerbear2008 <jeremyrwoolley@gmail.com>
2022-05-27 21:59:55 -0700

    and fix the apostrophe
    &apos;&apos;&apos;&apos;&apos;&apos;

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
   return (
     <Page title={false} showTitle={false} showContents={false}>
       <h1>The Out<i>site</i>rs</h1>
-      <p>This is my project on S. E. Hinton's <a href="https://en.wikipedia.org/wiki/The_Outsiders_(novel)">The Outsiders</a>. The pages are below.</p>
+      <p>This is my project on S. E. Hinton&apos;s <a href="https://en.wikipedia.org/wiki/The_Outsiders_(novel)">The Outsiders</a>. The pages are below.</p>
       <Contents />
     </Page>
   )
