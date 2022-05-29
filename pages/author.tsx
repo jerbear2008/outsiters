14f68624232ca4aaeca8226e22cd1124f99fba93
jerbear4328 <iamjerbear4328@gmail.com>
2022-05-29 10:22:21 -0700

    Fix typo

--- pages/author.tsx
+++ pages/author.tsx
@@ -3,7 +3,7 @@ import Page from '../components/page'
 
 const Author: NextPage = () => {
   return (
-    <Author title="Plot">
+    <Page title="Author">
       <p>The book was written by S. E. Hinton. S. E. Hinton was born on July 22, 1948. She started writing the book when she was fifteen years old, and published it when she graduated highschool. She is an avid reader, which is how she has learned to expertly convey emotion in her writing.</p>
     </Page>
   )
